945fabb30ec9356bb6abb1361940889cebfcb402
Tom Wagner <tom.wagner@xaxis.com>
2019-07-28 16:04:20 -0400

    Finish queue.py

--- assessment/queue.py
+++ assessment/queue.py
@@ -1,19 +1,43 @@
-# START TIME:
+# START TIME: 3:58pm
+
+from collections import deque
+
+# class Queue:
+#     def __init__(self):
+#         self.queue = []
+#         self.pointer = 0
+#
+#     def enqueue(self, item):
+#         self.queue.append(item)
+#
+#     def deque(self):
+#         if self.size == 0:
+#             return 'EMPTY'
+#         res = self.queue[self.pointer]
+#         self.pointer += 1
+#         return res
+#
+#     @property
+#     def size(self):
+#         return len(self.queue) - self.pointer
 
 
 class Queue:
     def __init__(self):
-        self.queue = []
+        self.queue = deque()
 
     def enqueue(self, item):
-        pass
+        self.queue.append(item)
 
     def deque(self):
-        pass
+        try:
+            return self.queue.popleft()
+        except:
+            return 'EMPTY'
 
     @property
     def size(self):
-        pass
+        return len(self.queue)
 
 
 q = Queue()
@@ -26,10 +50,10 @@ print(q.size == 3)
 
 print(q.deque() == 12)
 print(q.deque() == 14)
-
+#
 q.enqueue(18)
 q.enqueue(27)
-
+#
 print(q.deque() == 17)
 print(q.deque() == 18)
 
@@ -37,7 +61,7 @@ print(q.size == 1)
 
 print(q.deque() == 27)
 print(q.deque() == 'EMPTY')
+#
+# print(q.size == 0)
 
-print(q.size == 0)
-
-# END TIME:
+# END TIME: 4:04 PM
