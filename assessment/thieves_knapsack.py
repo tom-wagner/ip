f9f739a9e98191dc6b1eccfd952bbb054ef60a11
Tom Wagner <tom.wagner@xaxis.com>
2019-07-29 07:02:41 -0400

    Add thieves

--- assessment/thieves_knapsack.py
+++ assessment/thieves_knapsack.py
@@ -1,29 +1,54 @@
 # START TIME:
 
-from helpers.thieves_solution import thieves_solution
+from helpers.thieves_solution import knapsack01_dp, totalvalue
 
-# THIEVES KNAPSACK
+#    item, weight, value
+items = (
+    ("map", 9, 150),
+    ("compass", 13, 35),
+    ("water", 153, 200),
+    ("sandwich", 50, 160),
+    ("glucose", 15, 60),
+    ("tin", 68, 45),
+    ("banana", 27, 60),
+    ("apple", 39, 40),
+    ("cheese", 23, 30),
+    ("beer", 52, 10),
+    ("suntan cream", 11, 70),
+    ("camera", 32, 30),
+    ("t-shirt", 24, 15),
+    ("trousers", 48, 10),
+    ("umbrella", 73, 40),
+    ("waterproof trousers", 42, 70),
+    ("waterproof overclothes", 43, 75),
+    ("note-case", 22, 80),
+    ("sunglasses", 7, 20),
+    ("towel", 18, 12),
+    ("socks", 4, 50),
+    ("book", 30, 10),
+)
 
-#        NAME, WEIGHT, VALUE (for this weight)
-items = [("beef",    3.8, 36.0),
-         ("pork",    5.4, 43.0),
-         ("ham",     3.6, 90.0),
-         ("greaves", 2.4, 45.0),
-         ("flitch",  4.0, 30.0),
-         ("brawn",   2.5, 56.0),
-         ("welt",    3.7, 67.0),
-         ("salami",  3.0, 95.0),
-         ("sausage", 5.9, 98.0)]
-
-MAXWT = 15.0
+max_wt = 400
 
 
 def thieves(items, max_wt):
-    pass
+    return 1000
 
 
-ans = thieves_solution(items, MAXWT)
+def total_value(knapsack):
+    return 1000
 
-print(ans == thieves(items, MAXWT))
+
+thieves_answer = thieves(items, max_wt)
+thieves_total_value = total_value(thieves_answer)
+solution_bagged = knapsack01_dp(items, max_wt)
+total_value_solution = totalvalue(solution_bagged)
+
+
+print('my answer =', thieves_answer)
+print('solution =', solution_bagged)
+print('total value my items = ')
+print('solution total value =', total_value_solution)
+print('correct?', thieves_answer == solution_bagged)
 
 # END TIME:
