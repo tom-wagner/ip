00b13ea11d23c6394d5894b275776af73b4afca6
Tom Wagner <tom.wagner@xaxis.com>
2019-08-04 08:07:02 -0400

    Queue implementation

--- assessment/queue.py
+++ assessment/queue.py
@@ -1,19 +1,46 @@
-# START TIME:
+from collections import deque
 
+# START TIME: 8:02am
+
+
+# NATIVE IMPLEMENTATION:
+
+# class Queue:
+#     def __init__(self):
+#         self.queue = []
+#         self.pointer = 0
+#
+#     def enqueue(self, item):
+#         self.queue.append(item)
+#
+#     def deque(self):
+#         if self.pointer > len(self.queue) - 1:
+#             return 'EMPTY'
+#         temp = self.queue[self.pointer]
+#         self.pointer += 1
+#         return temp
+#
+#     @property
+#     def size(self):
+#         return len(self.queue) - self.pointer
 
 class Queue:
     def __init__(self):
-        self.queue = []
+        self.queue = deque()
+        self.pointer = 0
 
     def enqueue(self, item):
-        pass
+        self.queue.append(item)
 
     def deque(self):
-        pass
+        try:
+            return self.queue.popleft()
+        except IndexError:
+            return 'EMPTY'
 
     @property
     def size(self):
-        pass
+        return len(self.queue)
 
 
 q = Queue()
@@ -40,4 +67,4 @@ print(q.deque() == 'EMPTY')
 
 print(q.size == 0)
 
-# END TIME:
+# END TIME: 8:06am
