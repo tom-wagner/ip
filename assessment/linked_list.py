9d595122683ec1d566164baea743adb7e6c3882a
Tom Wagner <tom.wagner@xaxis.com>
2019-08-12 19:49:08 -0400

    Linked List

--- assessment/linked_list.py
+++ assessment/linked_list.py
@@ -1,30 +1,69 @@
+# start time: 7:37pm
+
+
+class Node:
+    def __init__(self, val):
+        self.val, self.next = val, None
+
+
 class LL:
     def __init__(self, arr):
-        self.list = None
+        self.h = self.t = None
+        self.init(arr)
+
+    def init(self, arr):
+        for x in arr:
+            self.add(x)
+
+    def as_list(self):
+        res = []
+        curr = self.h
+
+        while curr:
+            res.append(curr.val)
+            curr = curr.next
+
+        return res
 
     @property
     def size(self):
-        pass
+        return len(self.as_list())
 
     @property
     def head(self):
-        pass
+        return self.h.val
 
     @property
     def tail(self):
-        pass
+        return self.t.val
 
     def contains(self, x):
-        pass
+        return x in set(self.as_list())
 
     def add(self, x):
-        pass
+        if not self.h:
+            self.h = self.t = Node(x)
+            return
+        self.t.next = Node(x)
+        self.t = self.t.next
 
     def remove(self, x):
-        pass
+        head = self.h
+        if head.val == x:
+            self.h = self.h.next
+
+        curr, prev = head.next, head
+        while curr:
+            print(curr.val, prev.val)
+            if curr.val == x:
+                prev.next = curr.next
+                break
+            curr, prev = curr.next, prev.next
 
 
 ll = LL([1, 2, 3, 4, 5])
+
+print(ll.as_list())
 
 print('ll should contain 5', ll.contains(5))
 print('ll should not contain 8', not ll.contains(8))
@@ -41,4 +80,6 @@ print('ll tail = 5', ll.tail == 7)
 ll.remove(1)
 ll.remove(4)
 
-print('ll removed should not include 1 or 4', ll.list)
+print('ll removed should not include 1 or 4', ll.as_list())
+
+# end time: 7:48pm
