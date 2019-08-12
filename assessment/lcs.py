e16f273aa85e98ecedc77251300aca31ed3b12be
Tom Wagner <tom.wagner@xaxis.com>
2019-08-11 19:01:08 -0500

    LCS

--- assessment/lcs.py
+++ assessment/lcs.py
@@ -1,13 +1,22 @@
-# START TIME:
+# START TIME: 6:51pm
 
 
 def lcs(x: str, y: str):
-    return ''
+    matrix, longest = [[0 for _ in range(len(y))] for _ in range(len(x))], ''
+    for i, char_x in enumerate(x):
+        for j, char_y in enumerate(y):
+            if char_x == char_y:
+                preceding_length = matrix[i - 1][j - 1] if i > 0 and j > 0 else 0
+                new_length = preceding_length + 1
+                matrix[i][j] = new_length
+                if new_length > len(longest):
+                    longest = x[i - preceding_length:i + 1]
+    return longest
 
 
-res = lcs('careful', 'car')
+res = lcs('car', 'racecar')
 
 print('res: ', res)
 print(res == 'car')
 
-# END TIME: 
+# END TIME: 7:01pm
