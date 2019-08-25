9ec01558fee4b68eb3b25cd48136b756e05a6770
Tom Wagner <tom.wagner@xaxis.com>
2019-08-25 14:58:04 -0400

    BST

--- assessment/bst.py
+++ assessment/bst.py
@@ -1,30 +1,100 @@
-# START TIME:
+# START TIME: 2:40pm
+
+from collections import deque
+
+
+class Node:
+    def __init__(self, item):
+        self.val, self.left, self.right = item, None, None
 
 
 class BST:
     def __init__(self, array):
-        pass
+        self.tree = None
+        self.init(array)
 
-    def add(self, item):
-        pass
+    def init(self, array):
+        for x in array:
+            self.add(x)
+
+    def add(self, item, should_add: bool = True, should_return_branch: bool = False):
+        if not self.tree:
+            self.tree = Node(item)
+            return
+
+        curr, branch = self.tree, []
+
+        while curr:
+            branch.append(curr.val)
+            if item < curr.val:
+                if not curr.left:
+                    if should_add:
+                        curr.left = Node(item)
+                    return branch if should_return_branch else None
+                curr = curr.left
+            elif item > curr.val:
+                if not curr.right:
+                    if should_add:
+                        curr.right = Node(item)
+                    return branch if should_return_branch else None
+                curr = curr.right
+            else:
+                raise Exception
+
+    def find_location_and_return_branch(self, item):
+        """wrapper around add to specifically return the branch without adding"""
+        return self.add(item, should_add=False, should_return_branch=True)
 
     def largest_smaller_than_x(self, x):
-        pass
+        branch = self.find_location_and_return_branch(x)
+        return max([val for val in branch if val < x])
 
     def smallest_larger_than_x(self, x):
-        pass
+        branch = self.find_location_and_return_branch(x)
+        return min([val for val in branch if val > x])
+
+    def depth_first(self, traversal_type: str = 'IN_ORDER'):
+        res = []
+
+        def recurse(tree):
+            nonlocal res
+            if traversal_type == 'PRE_ORDER':
+                res.append(tree.val)
+            if tree.left:
+                recurse(tree.left)
+            if traversal_type == 'IN_ORDER':
+                res.append(tree.val)
+            if tree.right:
+                recurse(tree.right)
+            if traversal_type == 'POST_ORDER':
+                res.append(tree.val)
+
+        recurse(self.tree)
+        return res
 
     def in_order(self):
-        pass
+        return self.depth_first(traversal_type='IN_ORDER')
 
     def pre_order(self):
-        pass
+        return self.depth_first(traversal_type='PRE_ORDER')
 
     def post_order(self):
-        pass
+        return self.depth_first(traversal_type='POST_ORDER')
 
     def breadth_first(self):
-        pass
+        res = []
+        q = deque([self.tree])
+
+        while q:
+            curr = q.popleft()
+            res.append(curr.val)
+            if curr.left:
+                q.append(curr.left)
+            if curr.right:
+                q.append(curr.right)
+
+        return res
+
 
 arr = [63, 84, 57, 47, 50, 99, 59, 16, 39, 86, 2, 91, 12, 97]
 
@@ -58,4 +128,4 @@ print('largest smaller than correct', b.largest_smaller_than_x(77) == 72)
 # check that it returns proper value
 print('smallest larger than correct', b.smallest_larger_than_x(43) == 47)
 
-# END TIME: 7:57am
+# END TIME: 2:57pm
